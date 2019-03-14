790418b7b5e24eb2f4b56a009bf92bdfee4c4548
juliane-dpa <goersch.juliane@dpa.com>
2019-03-14 14:46:16 +0100

    changed data and heatmap

--- public/heatmap_orig.js
+++ public/heatmap_orig.js
@@ -1,6 +1,6 @@
 
-function drawCircos(error, months, electricalConsumption, daysOff, daysOffneu, daysOffend) {
-  var width = "800";
+function drawCircos(error, months, electricalConsumption, daysOff) {
+  var width = "600";
   var circosHeatmap = new Circos({
         container: '#heatmapChart',
         width: width,
@@ -16,22 +16,6 @@ function drawCircos(error, months, electricalConsumption, daysOff, daysOffneu, d
       };
     })
     daysOff = daysOff.map(function(d) {
-      return {
-        block_id: d.month,
-        start: parseInt(d.start),
-        end: parseInt(d.end),
-        value: parseFloat(d.value)
-      };
-    })
-    daysOffneu = daysOffneu.map(function(d) {
-      return {
-        block_id: d.month,
-        start: parseInt(d.start),
-        end: parseInt(d.end),
-        value: parseFloat(d.value)
-      };
-    })
-    daysOffend = daysOffend.map(function(d) {
       return {
         block_id: d.month,
         start: parseInt(d.start),
@@ -67,30 +51,16 @@ function drawCircos(error, months, electricalConsumption, daysOff, daysOffneu, d
         }
       })
       .heatmap('days-off', daysOff, {
-        innerRadius: 0.55,
-        outerRadius: 0.75,
-        logScale: false,
-        color: 'Blues'
-      })
-      .heatmap('days-off-neu', daysOffneu, {
-        innerRadius: 0.35,
-        outerRadius: 0.5,
+        innerRadius: 0.7,
+        outerRadius: 0.79,
         logScale: false,
         color: 'Reds'
-      })
-      .heatmap('days-off-end', daysOffend, {
-        innerRadius: 0.15,
-        outerRadius: 0.3,
-        logScale: false,
-        color: 'Blues'
       })
       .render()
 }
 
 d3.queue()
   .defer(d3.json, './months.json')
-  .defer(d3.csv, './circle_1_ersatz.csv')
-  .defer(d3.csv, './circle_3_ersatz.csv')
-  .defer(d3.csv, './circle_4_ersatz.csv')
-  .defer(d3.csv, './circle_5_ersatz.csv?ts=<?= time()')
+  .defer(d3.csv, './circle_1.csv')
+  .defer(d3.csv, './circle_2.csv')
   .await(drawCircos)
