85f083d6faf97b3feecaa0044bb65b315a6fb038
juliane-dpa <goersch.juliane@dpa.com>
2019-03-14 14:30:02 +0100

    changed for netlify

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,12 +3,12 @@
 {
   # Webbrowser after 3 seconds, livereload should have started
   sleep 3 
-  xdg-open http://0.0.0.0:18060 
+  open http://0.0.0.0:18060
 } &
 
 # Reload on file change
 # Install https://github.com/lepture/python-livereload first
-livereload --host 0.0.0.0 -p 18060 
+livereload --host 0.0.0.0 -p 18060 public/
 
 
 
