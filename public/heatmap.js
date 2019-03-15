4e0da84f144490bceec4dc90588d6c1b5aceca32
juliane-dpa <goersch.juliane@dpa.com>
2019-03-15 10:14:34 +0100

    added stylesheet, changed colors

--- public/heatmap.js
+++ public/heatmap.js
@@ -59,7 +59,7 @@ function drawCircos(error, months, Circle2014, Circle2015, Circle2016, Circle201
         innerRadius: 0.8,
         outerRadius: 0.98,
         logScale: false,
-        color: 'YlOrRd',
+        color: 'Purples',
         events: {
           'mouseover.demo': function (d, i, nodes, event) {
             console.log(d, i, nodes, event)
@@ -70,25 +70,25 @@ function drawCircos(error, months, Circle2014, Circle2015, Circle2016, Circle201
         innerRadius: 0.55,
         outerRadius: 0.75,
         logScale: false,
-        color: 'Blues'
+        color: 'Purples'
       })
       .heatmap('Circle2016', Circle2016, {
         innerRadius: 0.35,
         outerRadius: 0.5,
         logScale: false,
-        color: 'Reds'
+        color: 'Purples'
       })
       .heatmap('Circle2017', Circle2017, {
         innerRadius: 0.15,
         outerRadius: 0.3,
         logScale: false,
-        color: 'Blues'
+        color: 'Purples'
       })
       .render()
 }
 
 d3.queue()
-  .defer(d3.json, './months.json')
+  .defer(d3.json, './months.json?')
   .defer(d3.csv, './circle2014.csv')
   .defer(d3.csv, './circle2015.csv')
   .defer(d3.csv, './circle2016.csv')
